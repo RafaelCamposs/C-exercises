0a16c82fe2e5f5a58d0a7a4d9d98e7c4e0505946
Rafael Campos <rafacampos2000@hotmail.com>
2019-11-13 12:44:25 -0300

    Cadastro de alunos v0.2

--- cadastrarAlunos.c
+++ cadastrarAlunos.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#define TAMANHO_LISTA 10
 
 struct dataNascimento
 {
@@ -8,58 +10,170 @@ struct dataNascimento
     int ano;
 };
 
-struct cadAluno
+typedef struct cadAluno
 {
     char nome[50];
     struct dataNascimento dtnasc;
     char CPF[15];
     int matricula;//ano depois numero, ex: 201901,201902
     char sexo;
-};
+} Lista_aluno;
 
-struct cadAluno Aluno[15];
-void cadastroAluno()
+
+int validarnome(char nm[50]);
+int validardtnasc(int d, int m, int a);
+int validarcpf(char c[15]);
+int validarsexo(char s);
+int validarmatricula(int mat);
+int matricula_Aluno(int qtd,Lista_aluno Alunos[]);
+int menu_opcoes();
+void remover_Aluno();
+
+int main()
 {
-    int doagain;
-    int numAlunos;
-    printf("...Cadastro de Aluno...\n");
-    
-    
-    numAlunos=0;
-    doagain=1;
-    for(;doagain==1;numAlunos++)
+    int x;
+    int opcao;
+    int status_matricula;
+    int qtdaluno=0;
+    Lista_aluno Aluno[TAMANHO_LISTA];
+    for(x=0;x<10;x++)
     {
-        printf("Informe o nome do Aluno\n");
-        fgets(Aluno[numAlunos].nome,50,stdin);
-        printf("Informe o dia do nascimento\n");
-        scanf("%d",&Aluno[numAlunos].dtnasc.dia);
-        printf("Informe o mes do nascimento\n");
-        scanf("%d",&Aluno[numAlunos].dtnasc.mes);
-        printf("Informe o ano do nascimento\n");
-        scanf("%d",&Aluno[numAlunos].dtnasc.ano);
-        printf("Informe o Numero da matricula\n");
-        scanf("%d",&Aluno[numAlunos].matricula);
-        printf("Informe o CPF\n");
-        scanf("%s",&Aluno[numAlunos].CPF);
-        printf("Informe o sexo do Aluno\n");
-        scanf("%s",&Aluno[numAlunos].sexo);
-        printf("Aluno cadastrado com sucesso!\n");
-        printf("Para continuar digite 1, para sair digite 2\n");
-        scanf("%d",&doagain);
+        status_matricula = matricula_Aluno(qtdaluno,Lista_aluno Aluno[TAMANHO_LISTA]);
+        if(status_matricula==1)
+        {
+            printf("### MATRICULA REALIZADA COM SUCESSO###\n");
+            qtdaluno++;
+        }
+        else
+            printf("###FALHA NA MATRICULA###\n");
     }
-            
-    return;
+    return 0;    
+    
 }
    
-void main()
+            
+
+int matricula_Aluno(int qtd, Lista_aluno Alunos[])
 {
-    cadastroAluno();
-    printf("Cadastros Feitos com sucesso\n");
-    printf("Lista de alunos\n");
-    printf(" Nome do aluno 1:%s",Aluno[0].nome);
-    printf("Data de nascimento :%d/%d/%d",Aluno[0].dtnasc.dia,Aluno[0].dtnasc.mes,Aluno[0].dtnasc.ano);
-    printf("Matricula : %d",Aluno[0].matricula);
-    printf("CPF ; %s",Aluno[0].CPF);
-    printf("Sexo : %s",Aluno[0].sexo);
-    return;
+    int v1,v2,v3,v4,v5;
+    
+    printf("Informe o nome do Aluno\n");
+    fgets(Alunos[qtd].nome,50,stdin);
+    size_t ln= strlen(Alunos[qtd].nome)-1;
+    if(Alunos[qtd].nome[ln]=='\n')
+            Alunos[qtd].nome[ln]='\0';
+    
+    printf("Informe a dia do Nascimento\n");
+    scanf("%d",&Alunos[qtd].dtnasc.dia);
+    
+    printf("Informe a mes do Nascimento\n");
+    scanf("%d",&Alunos[qtd].dtnasc.mes);
+    
+    printf("Informe a ano do Nascimento\n");
+    scanf("%d",&Alunos[qtd].dtnasc.ano);
+        
+    printf("Informe a matricula do aluno\n");
+    scanf("%d",&Alunos[qtd].matricula);
+
+    printf("Informe o CPF do Aluno\n");
+    scanf("%s",&Alunos[qtd].CPF);
+        
+    printf("Infome o sexo do Aluno(F, M ou O)\n");
+    scanf("%s",&Alunos[qtd].sexo);
+    
+    //Validação dos dados inseridos//
+    v1=validarnome(Alunos[qtd].nome);
+    v2=validardtnasc(Alunos[qtd].dtnasc.dia,Alunos[qtd].dtnasc.mes,Alunos[qtd].dtnasc.ano);
+    v3=validarcpf(Alunos[qtd].CPF);
+    v4=validarsexo(Alunos[qtd].sexo);
+    v5=validarmatricula(Alunos[qtd].matricula);
+    
+    if(v1==1 && v2==1 && v3==1 && v4==1 && v5==1)
+    {
+        return 1;
+    }
+    if(v1==0)
+        printf("ERRO_NOME_INVALIDO\n");
+    if(v2==0)
+        printf("ERRO_DATA_INVALIDA\n");
+    if(v3==0)
+        printf("ERRO_CPF_INVALIDO\n");
+    if(v4==0)
+        printf("ERR0_SEXO_INVALIDO\n");
+    if(v5==0)
+        printf("ERRO_MATRICULA_INVALIDA\n");
+    return 0;
+    
 }
+
+int validarnome(char nm[])
+{
+    int tam;
+    tam=strlen(nm);
+    if(tam>20)
+        return 1;
+     else
+        return 0;
+}
+
+int validardtnasc(int d,int m,int a)
+{
+    if( d<=31 &&  d>0)
+    {
+        if( m<=12 &&  m>0)
+        {
+            if( a > 0)
+                return 1;
+            else 
+                return 0;
+        }
+        else
+            return 0;
+    }
+    else
+        return 0;
+}
+
+int validarcpf(char c[])
+{
+    int len;
+    len = strlen(c);
+    if(len==14 || len==11)
+        return 1;
+    else
+        return 0;
+}
+
+int validarsexo(char s)
+{
+    if(s=='M' || s=='m' || s=='F' || s=='f' || s=='O'  || s=='o')
+        return 1;
+    else
+        return 0;
+}
+
+int validarmatricula(int mat)
+{
+    if(mat>0)
+        return 1;
+    else
+        return 0;
+}
+
+int menu_ocpoes()
+{
+    int op;
+    
+    printf("###CADASTRO DE ALUNOS###\n");
+    printf("Informe a opcao\n");
+    printf("1.Cadastro de Alunos\n");
+    printf("2.Remover Alunos\n");
+    printf("3.Relatórios de Alunos\n");
+    printf("4.Sair\n");
+    scanf("%d",&op);
+    return op;
+}
+
+
+
+
