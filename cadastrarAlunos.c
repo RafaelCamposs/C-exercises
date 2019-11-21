dd885ff6e6c089a27704017aadd90c4c8c5e32a9
Rafael Campos <rafacampos2000@hotmail.com>
2019-11-21 14:50:24 -0300

    Adicionando função de alterar informações

--- cadastrarAlunos.c
+++ cadastrarAlunos.c
@@ -30,6 +30,7 @@ int menu_opcoes();
 void listar_aluno(Lista_aluno Alunos[]);
 void status_aluno(Lista_aluno Alunos[]);
 void retirar_aluno(int qtd,Lista_aluno Alunos[]);
+void alterar_aluno(Lista_aluno Alunos[]);
 
 int main()
 {
@@ -41,7 +42,7 @@ int main()
     //inicializando as matriculas como inativas
     status_aluno(Aluno);
     
-    while(opcao!=4)
+    while(opcao!=5)
     {
         opcao=menu_opcoes();
         switch(opcao)
@@ -88,7 +89,13 @@ int main()
             }
             case 4:
             {
-                printf("\n###SAINDO DO CADASTRO DE ALUNOS###\n");
+                printf("\n###ALTERAR DADOS DE ALUNOS###\n");
+                alterar_aluno(Aluno);
+                break;
+            }
+            case 5:
+            {
+                printf("\n###SAINDO DO MENU DE ALUNOS###\n");
                 break;
             }
         
@@ -232,9 +239,10 @@ int menu_opcoes()
     printf("Informe a opcao\n");
     printf("\n");
     printf("1.Cadastro de Alunos\n");
-    printf("2.Relatórios de Alunos\n");
+    printf("2.Listar Alunos\n");
     printf("3.Remover Alunos\n");
-    printf("4.Sair\n");
+    printf("4.Alterar dados de Alunos\n");
+    printf("5.Sair\n");
     scanf("%d",&op);
     return op;
 }
@@ -287,4 +295,133 @@ void retirar_aluno(int qtd,Lista_aluno Alunos[])
     listar_aluno(Alunos);
     return ;
 }
+
+void alterar_aluno(Lista_aluno Alunos[])
+{
+    int alt;
+    int op;
+    int ver;
+    int altnum,altdia,altmes,altano;
+    char altname[50];
+    char altchar;
+    
+    printf("Informe o Aluno que quer alterar\n");
+    listar_aluno(Alunos);
+    scanf("%d",&alt);
+    alt--;
+    printf("Informe que dado alterar\n");
+    printf("1.Matricula\n");
+    printf("2.Nome\n");
+    printf("3.Data de Nascimento\n");
+    printf("4.CPF\n");
+    printf("5.Sexo\n");
+    scanf("%d",&op);
+    getchar();
+    
+    switch(op)
+    {
+        case 1:
+        {
+            printf("Informe a nova matricula\n");
+            scanf("%d",&altnum);
+            ver=validarmatricula(altnum);
+            if(ver==1)
+            {
+                Alunos[alt].matricula=altnum;
+                printf("\n###ALTERAÇÃO REALIZADA COM SUCESSO###\n");
+                break;
+            }
+            else
+            {
+                printf("\n###ERRO_MATRICULA_INVALIDA###\n");
+                break;
+            }
+        }
+        case 2:
+        {
+            printf("Informe o novo nome\n");
+            fgets(altname,50,stdin);
+            size_t ln= strlen(altname)-1;
+            if(altname[ln]=='\n')
+                altname[ln]='\0';    
+            ver=validarnome(altname);
+            if(ver==1)
+            {
+                strcpy(Alunos[alt].nome,altname);
+                printf("\n###ALTERAÇÃO REALIZADA COM SUCESSO###\n");
+                break;
+            }
+            else
+            {
+                printf("\n###ERRO_NOME_INVALIDO###\n");
+                break;
+            }
+        }
+        case 3:
+        {
+            printf("Informe a nova Data de Nascimento\n");
+            printf("Dia:\n");
+            scanf("%d",&altdia);
+            printf("Mes:\n");
+            scanf("%d",&altmes);    
+            printf("Ano:\n");
+            scanf("%d",&altano);
+            ver=validardtnasc(altdia,altmes,altano);
+            if(ver==1)
+            {
+                Alunos[alt].dtnasc.dia=altdia;
+                Alunos[alt].dtnasc.mes=altmes;
+                Alunos[alt].dtnasc.ano=altano;
+                printf("\n###ALTERAÇÃO REALIZADA COM SUCESSO###\n");
+                break;
+            }
+            else
+            {
+                printf("\n###ERRO_DATA_INVALIDA###\n");
+                break;
+            }
+        }
+        case 4:
+        {
+            printf("Informe o novo CPF\n");
+            fgets(altname,50,stdin);
+            size_t ln= strlen(altname)-1;
+            if(altname[ln]=='\n')
+                altname[ln]='\0';    
+            ver=validarcpf(altname);
+            if(ver==1)
+            {
+                strcpy(Alunos[alt].CPF,altname);
+                printf("\n###ALTERAÇÃO REALIZADA COM SUCESSO###\n");
+                break;
+            }
+            else
+            {
+                printf("\n###ERRO_CPF_INVALIDO###\n");
+                break;
+            }
+        } 
+        case 5:
+        {
+            printf("Informe o novo sexo\n");
+            scanf("%c",&altchar);
+            ver=validarsexo(altchar);
+            if(ver==1)
+            {
+                Alunos[alt].sexo=altchar;
+                printf("\n###ALTERAÇÃO REALIZADA COM SUCESSO###\n");
+                break;
+            }
+            else
+            {
+                printf("\n###ERRO_SEXO_INVALIDA###\n");
+                break;
+            }
+        }
+    }
+       
+    return;
+}
+            
+    
     
